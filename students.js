20691b2ebe89590d7f3d9efa7514a8635c09cf19
e.finogeev <e.finogeev@iteco-inno.ru>
2020-04-19 13:44:59 +0300

    Import

--- students.js
+++ students.js
@@ -1,10 +1,17 @@
 // Let's use some ES6 features
-const averageScore = "90";
-const messageToDisplay = `average score ${averageScore}`;
+import { getAvg, getTotalScore } from "./averageService";
+
+const scores = [90, 75, 60, 99, 94, 30];
+const averageScore = getAvg(scores);
+const totalScore = getTotalScore(scores);
+
+const messageToDisplayAvg = `average score ${averageScore} `;
+const messageToDisplayTotal = `total score ${totalScore}`;
 
 new Promise((resolve, reject) =>
   window.setTimeout(() => {
-    document.write(messageToDisplay);
+    document.write(messageToDisplayAvg);
+    document.write(messageToDisplayTotal);
     resolve();
   }, 2000)
 );
